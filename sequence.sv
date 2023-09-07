a084b361172f465608540768c07519cf5d0486f1
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2023-09-07 11:38:50 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -3,21 +3,27 @@ class sequence_item extends uvm_sequence_item;
   //---------------------------------------
   //Input and Output data
   //---------------------------------------
-  rand bit [7:0]data_in;
-  rand bit rd;
-  rand bit wr;
-  bit full;
-  bit empty;
-  bit [7:0]data_out;
+  rand bit [DATA_W-1:0]i_wrdata;
+  rand bit i_rden ;
+  rand bit i_wren;
+  bit o_full;
+  bit o_empty;
+  bit o_alm_full;
+  bit o_alm_empty;
+  bit [DATA_W-1:0]o_rddata;
   
   //---------------------------------------
   //Utility and Field macros
   //---------------------------------------
-  `uvm_object_utils_begin(fifo_seq_item)
-  `uvm_field_int(data_in, UVM_ALL_ON)
-  `uvm_field_int(rd, UVM_ALL_ON)
-  `uvm_field_int(wr, UVM_ALL_ON)
-  `uvm_field_int(full, UVM_ALL_ON)
-  `uvm_field_int(empty, UVM_ALL_ON)
-  `uvm_field_int(data_out, UVM_ALL_ON)
+  `uvm_object_utils_begin(sequence_item)
+  `uvm_field_int(i_wrdata, UVM_ALL_ON)
+  `uvm_field_int(i_rden, UVM_ALL_ON)
+  `uvm_field_int(i_wren, UVM_ALL_ON)
+  `uvm_field_int(o_full, UVM_ALL_ON)
+  `uvm_field_int(o_empty, UVM_ALL_ON)
+  `uvm_field_int(o_alm_full, UVM_ALL_ON)
+  `uvm_field_int(o_alm_empty, UVM_ALL_ON)
+  `uvm_field_int(o_rddata, UVM_ALL_ON)
   `uvm_object_utils_end
+
+  
