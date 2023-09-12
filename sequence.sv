150ee2d0a4b98dc38574d255f62f9dcb93539ccd
01sharanshetty <126483491+01sharanshetty@users.noreply.github.com>
2023-09-12 10:30:34 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -1,38 +1,57 @@
-class f_sequence extends uvm_sequence #(f_sequence_item);
 
-`uvm_object_utils(f_sequence)
 
- function new(string name = "f_sequence");
+//TEST_1: Generate 1024 continuous writes with no read operation
+
+ 
+class f_sequence_test1 extends uvm_sequence #(f_sequence_item);
+
+ `uvm_object_utils(f_sequence_test1)
+
+ f_sequence_item fsi;
+ 
+ function new(string name = "f_sequence_test1");
     super.new(name);
 endfunction
-//edit from here//
-    virtual task body();
-    `uvm_info(get_type_name(), $sformatf("******** Generate 16 Write REQs ********"), UVM_LOW)
-    repeat(16) begin
-      req = f_sequence_item::type_id::create("req");
-      start_item(req);
-      assert(req.randomize() with {wr == 1;});
-      finish_item(req);
-    end
-    `uvm_info(get_type_name(), $sformatf("******** Generate 16 Read REQs ********"), UVM_LOW)
-    repeat(16) begin
-      req = f_sequence_item::type_id::create("req");
-      start_item(req);
-      assert(req.randomize() with {rd == 1;});
-      finish_item(req);
-    end
-    `uvm_info(get_type_name(), $sformatf("******** Generate 20 Random REQs ********"), UVM_LOW)
-    repeat(20) begin
-      req = f_sequence_item::type_id::create("req");
-      start_item(req);
-      assert(req.randomize());
-      finish_item(req);
-    end
-  endtask
 
 
+ task body();
+  `uvm_info(get_type_name(), $sformatf("******** Generate 1024 continuous writes with no read operation ********"), UVM_LOW)
+  repeat(1024) begin
+   fsi= f_sequence_item::type_id::create("fsi_write");
+   start_item(fsi);
+   assert(fsi.randomize() with {fsi.i_wren == 1; fsi.i_rden == 0;});
+   finish_item(fsi);
+   end
 
-
-
-
+endtask
 endclass
+
+//TEST_2: Generate 1024 continuous read with no write operation
+
+class f_sequence_test2 extends uvm_sequence #(f_sequence_item);
+
+ `uvm_object_utils(f_sequence_test2)
+
+ f_sequence_item fsi;
+ 
+ function new(string name = "f_sequence_test2");
+    super.new(name);
+endfunction
+
+
+ task body();
+  `uvm_info(get_type_name(), $sformatf("******** Generate 1024 continuous read with no write operation ********"), UVM_LOW)
+  repeat(1024) begin
+   fsi= f_sequence_item::type_id::create("fsi_read");
+   start_item(fsi);
+   assert(fsi.randomize() with {fsi.i_wren == 0; fsi.i_rden == 1;});
+   finish_item(fsi);
+   end
+
+endtask
+endclass
+
+
+
+ 
+  
